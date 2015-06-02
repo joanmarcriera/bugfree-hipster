48fbe2ab86e59da6b0051a1206a3d668ea0bbee7
Joan Marc Riera Duocastella <joanmarcriera@gmail.com>
2015-06-02 20:31:34 +0200

    small

--- vagrant-box/bootstrap.sh
+++ vagrant-box/bootstrap.sh
@@ -15,4 +15,3 @@ wget -qO- https://get.docker.com/ | sh && sudo usermod -aG docker vagrant
 curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-
