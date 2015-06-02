7a18c81e0b2bdfa0a7cea8866e4c9f7c0aa90d16
Joan Marc Riera Duocastella <joanmarcriera@gmail.com>
2015-06-02 20:27:07 +0200

    Changed to ubuntu 15.04

--- vagrant-box/bootstrap.sh
+++ vagrant-box/bootstrap.sh
@@ -11,5 +11,8 @@ cd /home/vagrant
 # Install docker
 wget -qO- https://get.docker.com/ | sh && sudo usermod -aG docker vagrant
 
-sudo su
+# Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 
